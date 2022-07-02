aa01653ed24da3762e61d1acb298b64a54a3cb18
Natalia Shuvaeva <glukomani@gmail.com>
2022-07-02 23:14:33 +0200

    Husky and docker are added

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.static("./dist"));
+app.use(express.static('./dist'))
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+ console.log(`My app is listening on port ${PORT}!`)
+})
+
+app.get('/', function (req, res) {
+ res.send(__dirname + 'index.html')
+})
+
+// app.get('*', (req, res) => {
+//  res.sendFile(`${__dirname}/dist/index.html`)
+// })
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+// })
